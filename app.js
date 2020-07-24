0f74fa2b00940ff7b63f2bbcceb8eead5a4e7e8b
Anton Zmanovskii <antonzm34@gmail.com>
2020-07-24 16:49:46 +0400

    h

--- app.js
+++ app.js
@@ -26,53 +26,57 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
+app.get('/', (req, res) => {
+  res.send({ message: 'Вот так' });
 });
 
-app.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
-  }),
-  login // eslint-disable-line
-);
-app.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-      name: Joi.string().required().min(2).max(30),
-      about: Joi.string().required().min(2).max(30),
-      avatar: Joi.string().required().pattern(validateUrl),
-    }),
-  }),
-  createUser // eslint-disable-line
-);
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 
-app.use('/cards', auth, cardsRoute);
-app.use('/users', auth, usersRoute);
+// app.post(
+//   '/signin',
+//   celebrate({
+//     body: Joi.object().keys({
+//       email: Joi.string().required().email(),
+//       password: Joi.string().required().min(8),
+//     }),
+//   }),
+//   login // eslint-disable-line
+// );
+// app.post(
+//   '/signup',
+//   celebrate({
+//     body: Joi.object().keys({
+//       email: Joi.string().required().email(),
+//       password: Joi.string().required().min(8),
+//       name: Joi.string().required().min(2).max(30),
+//       about: Joi.string().required().min(2).max(30),
+//       avatar: Joi.string().required().pattern(validateUrl),
+//     }),
+//   }),
+//   createUser // eslint-disable-line
+// );
 
-app.use('*', (req, res, next) => {
-  next(new NotFoundError('Запрашиваемый ресурс не найден'));
-});
+// app.use('/cards', auth, cardsRoute);
+// app.use('/users', auth, usersRoute);
 
-app.use(errorLogger);
-app.use(errors());
+// app.use('*', (req, res, next) => {
+//   next(new NotFoundError('Запрашиваемый ресурс не найден'));
+// });
 
-/* eslint-disable*/
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
-  });
-});
+// app.use(errorLogger);
+// app.use(errors());
+
+// /* eslint-disable*/
+// app.use((err, req, res, next) => {
+//   const { statusCode = 500, message } = err;
+//   res.status(statusCode).send({
+//     message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`Приложение запущено на port:${PORT}`);
